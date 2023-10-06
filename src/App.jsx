e43cf3934944ba4c7af3333a47a9b29560603bc9
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-10-06 15:32:31 +0300

    duzenlemeler yapiliyor

---
+++ src/App.jsx
@@ -0,0 +1,53 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="navbar">
+       <a href="#"><img className="logo" src="https://cdn.armut.com/images/armut-logo-colour-negative@3x.png" alt="" /></a>
+      <ul className='linkler'>
+          <li>Temizlik</li>
+          <li>Tadilat</li>
+          <li>Nakliyat</li>
+          <li>Tamir</li>
+          <li>Özel Ders</li>
+          <li>Sağlık</li>
+          <li>Organizasyon</li>
+          <li>Diğer</li>
+      </ul>
+      </div>
+      <div className='button'>
+        <ul className='but-liste'>
+          <li>
+            <a href="" className='but'>YARDIM</a>
+          </li>
+          <li>
+            <a href="" className='but-yesil'>HİZMET VER</a>
+          </li>
+          <li>
+            <a href="" className='but'>GİRİŞ</a>
+          </li>
+        </ul>
+      </div>
+      <section>
+        <div className='hero back-img tr'>
+          <div className='back-img'></div>
+        </div>
+        <div className="wrapper">
+          <h1 className="widow-fix">Hizmet Piş, Ağzıma&nbsp;Düş</h1>
+          <h2 className="widow-fix">İhtiyacın olan hizmete kolayca ulaş, bekleyen işlerini&nbsp;hallet</h2>
+          
+        </div>
+      
+      </section>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
