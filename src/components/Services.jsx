5b9f4144c549631164db67c47cb13ae383dbfd94
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-12-11 21:19:36 +0300

    duzenleme yapiliyor

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,29 +1,61 @@
 import "/src/App.css"
 export default function Services() {
     return (
+        <div className="hard-brad">
+            <div className="services">
+                <div className="servis-first">
+                    <span>1</span>
+                    <h2>İHTİYACINI BİRKAÇ SORUDAN ANLAT</h2>
+                    <span className="green-line"></span>
+                    <p>İhtiyacına özel soruları cevapla ve iletişim bilgilerini gir. İstersen iletişim bilgilerini gizli tut.</p>
+                </div>
 
-        <div className="services">
-            <div className="servis-first">
-                <span>1</span>
-                <h2>İHTİYACINI BİRKAÇ SORUDAN ANLAT</h2>
-                <span className="green-line"></span>
-                <p>İhtiyacına özel soruları cevapla ve iletişim bilgilerini gir. İstersen iletişim bilgilerini gizli tut.</p>
+                <div className="servis-second">
+                    <span>2</span>
+                    <h2>ÖZEL FİYAT TEKLİFLERİ AL</h2>
+                    <span className="green-line"></span>
+                    <p>İşinle ilgilenen ve müsait olan profesyonellere ihtiyacını bildirelim, ihtiyacına özel teklif al.</p>
+                </div>
+
+                <div className="servis-third">
+                    <span>3</span>
+                    <h2>KARŞILAŞTIR VE SEÇ</h2>
+                    <span className="green-line"></span>
+                    <p>Müşteri yorumlarını incele, pazarlık yap. En uygun fiyat ve kaliteyi tercih et.</p>
+                </div>
             </div>
 
-            <div className="servis-second">
-                <span>2</span>
-                <h2>ÖZEL FİYAT TEKLİFLERİ AL</h2>
-                <span className="green-line"></span>
-                <p>İşinle ilgilenen ve müsait olan profesyonellere ihtiyacını bildirelim, ihtiyacına özel teklif al.</p>
-            </div>
+            <section>
+                  <h6 className='armutFooter'>Bilgi</h6>
+                  <nav className='footerFirstStatemenet' >
+                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
+                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
+                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
+                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
+                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                  </nav>
+                </section>
 
-            <div className="servis-third">
-                <span>3</span>
-                <h2>KARŞILAŞTIR VE SEÇ</h2>
-                <span className="green-line"></span>
-                <p>Müşteri yorumlarını incele, pazarlık yap. En uygun fiyat ve kaliteyi tercih et.</p>
-            </div>
-
+                <section>
+                  <h6 className='armutFooter'>Bilgi</h6>
+                  <nav className='footerFirstStatemenet' >
+                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
+                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
+                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
+                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
+                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                  </nav>
+                </section>
+                <section>
+                  <h6 className='armutFooter'>Bilgi</h6>
+                  <nav className='footerFirstStatemenet' >
+                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
+                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
+                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
+                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
+                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                  </nav>
+                </section>
         </div>
     )
 }
