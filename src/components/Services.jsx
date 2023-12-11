062ee2aae9025eb52df1b20d68e64e15fc1bea60
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-12-11 11:47:01 +0300

    servis hazirlaniyor

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -3,6 +3,26 @@ export default function Services() {
     return (
 
         <div className="services">
+            <div className="servis-first">
+                <span>1</span>
+                <h2>İHTİYACINI BİRKAÇ SORUDAN ANLAT</h2>
+                <span className="green-line"></span>
+                <p>İhtiyacına özel soruları cevapla ve iletişim bilgilerini gir. İstersen iletişim bilgilerini gizli tut.</p>
+            </div>
+
+            <div className="servis-first">
+                <span>2</span>
+                <h2>ÖZEL FİYAT TEKLİFLERİ AL</h2>
+                <span className="green-line"></span>
+                <p>İşinle ilgilenen ve müsait olan profesyonellere ihtiyacını bildirelim, ihtiyacına özel teklif al.</p>
+            </div>
+
+            <div className="servis-first">
+                <span>3</span>
+                <h2>KARŞILAŞTIR VE SEÇ</h2>
+                <span className="green-line"></span>
+                <p>Müşteri yorumlarını incele, pazarlık yap. En uygun fiyat ve kaliteyi tercih et.</p>
+            </div>
 
         </div>
     )
