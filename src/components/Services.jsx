0ecc6d1cf071a475efa04616054a29d5f009827c
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-12-12 15:39:03 +0300

    duzenleme yapiliyor

--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -24,38 +24,70 @@ export default function Services() {
                     <p>Müşteri yorumlarını incele, pazarlık yap. En uygun fiyat ve kaliteyi tercih et.</p>
                 </div>
             </div>
-
+                <h2>POPÜLER HİZMETLER</h2>
             <section>
-                  <h6 className='armutFooter'>Bilgi</h6>
                   <nav className='footerFirstStatemenet' >
-                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
-                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
-                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
-                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
-                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                    <a href="#"><p2 className="statment">Boş Ev Temizliği</p2><br /></a>
+                    <a href="#"><p2 className="statment">Boyacı (Boya Badana Ustası)</p2><br /></a>
+                    <a href="#"><p2 className="statment">Demirdöküm Kombi Servisi</p2><br />  </a>
+                    <a href="#"><p2 className="statment">Direksiyon Dersi</p2><br /></a>
+                    <a href="#"><p2 className="statment">Duvar Dekorasyonu</p2></a>
+                    <a href="#"><p2 className="statment">Elektrik</p2></a>
                   </nav>
                 </section>
 
                 <section>
-                  <h6 className='armutFooter'>Bilgi</h6>
                   <nav className='footerFirstStatemenet' >
-                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
-                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
-                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
-                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
-                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                    <a href="#"><p2 className="statment">Elektrikçi</p2><br /></a>
+                    <a href="#"><p2 className="statment">Parça Eşya Taşıma</p2><br /></a>
+                    <a href="#"><p2 className="statment">Ev Dekorasyonu</p2><br />  </a>
+                    <a href="#"> <p2 className="statment">Ev Temizliği</p2><br /></a>
+                    <a href="#"><p2 className="statment">Evden Eve Nakliyat</p2></a>
+                    <a href="#"><p2 className="statment">Fayans Döşeme</p2></a>
                   </nav>
                 </section>
                 <section>
-                  <h6 className='armutFooter'>Bilgi</h6>
                   <nav className='footerFirstStatemenet' >
-                    <a href="#"><p2 className="statment">Nasıl Çalışır?</p2><br /></a>
-                    <a href="#"><p2 className="statment">İletişim</p2><br /></a>
-                    <a href="#"><p2 className="statment">Blog </p2><br />  </a>
-                    <a href="#"> <p2 className="statment">Hakkımızda</p2><br /></a>
-                    <a href="#"><p2 className="statment">Kariyer</p2></a>
+                    <a href="#"><p2 className="statment">Fotoğrafçı</p2><br /></a>
+                    <a href="#"><p2 className="statment">Grafik Tasarımı</p2><br /></a>
+                    <a href="#"><p2 className="statment">Halı Yıkama Temizleme</p2><br />  </a>
+                    <a href="#"> <p2 className="statment">İç mimar</p2><br /></a>
+                    <a href="#"><p2 className="statment">İngilizce Özel Dersi</p2></a>
+                    <a href="#"><p2 className="statment">Koltuk Yıkama Temizleme</p2></a>
                   </nav>
                 </section>
+
+                <div className="yakin-hizmet">
+                        <h2>EN YAKIN HİZMETLER</h2>
+                    <section>
+                        <nav className='footerFirstStatemenet' >
+                            <a href="#"><p2 className="statment">istanbul</p2><br /></a>
+                            <a href="#"><p2 className="statment">Adana</p2><br /></a>
+                            <a href="#"><p2 className="statment">Balıkesir</p2><br />  </a>
+                            <a href="#"> <p2 className="statment">Eskişehir</p2><br /></a>
+                            <a href="#"><p2 className="statment">Muğla</p2></a>
+                        </nav>
+                    </section>
+                    <section>
+                        <nav className='footerFirstStatemenet' >
+                            <a href="#"><p2 className="statment">Ankara</p2><br /></a>
+                            <a href="#"><p2 className="statment">Antalya</p2><br /></a>
+                            <a href="#"><p2 className="statment">Bursa</p2><br />  </a>
+                            <a href="#"> <p2 className="statment">Kocaeli</p2><br /></a>
+                            <a href="#"><p2 className="statment">Samsun</p2></a>
+                        </nav>
+                    </section>
+                    <section>
+                        <nav className='footerFirstStatemenet' >
+                            <a href="#"><p2 className="statment">İzmir</p2><br /></a>
+                            <a href="#"><p2 className="statment">Aydın</p2><br /></a>
+                            <a href="#"><p2 className="statment">Çanakkale</p2><br />  </a>
+                            <a href="#"> <p2 className="statment">Konya</p2><br /></a>
+                            <a href="#"><p2 className="statment">Tekirdağ</p2></a>
+                        </nav>
+                    </section>
+                </div>
+                <h1>HAYATINI KOLAYLAŞTIR</h1> <button>ARA</button>
         </div>
     )
 }
