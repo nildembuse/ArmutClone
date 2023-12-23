a97a87779ca281b132307e017606d52691cb5316
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-12-23 15:25:57 +0300

    login islemi supabase baglama

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,9 @@ import './App.css'
 import { RouterProvider,createBrowserRouter } from 'react-router-dom'
 import Header from './components/Header.jsx'
 import Home from './components/Home.jsx'
+import Login from './components/Login.jsx'
+import { createClient } from '@supabase/supabase-js'
+export const supabase = createClient('https://ridqsymwdsixvcfadgfa.supabase.co', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpZHFzeW13ZHNpeHZjZmFkZ2ZhIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTk4NzQ3MzEsImV4cCI6MjAxNTQ1MDczMX0.G8HWBXxpqRJadygtrKOMZ1DI7hE-m_7fGzbs5H3EFp4')
 
 
 const router = createBrowserRouter (
@@ -20,6 +23,10 @@ const router = createBrowserRouter (
        
       ]
     },
+    {
+      path:'/login',
+      element:<Login />
+  },
    
   ]
   
