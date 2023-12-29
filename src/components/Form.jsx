0ab88b1d81c4daf2c4a45d10661a0bde60f49ef9
Nildem Buse Ertürk <nildembuse@gmail.com>
2023-12-29 20:44:45 +0300

    form ekleniyor

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,7 +1,27 @@
-function Form(){
-    return(
-        <form>
+import { useState } from "react"
 
+function Form(){
+    const [step, setStep] = useState(1)
+
+    function handleSubmit(e) {
+        e.preventDefault();
+    }
+
+    return(
+        <form className="formDuzen" onSubmit={handleSubmit}>
+            {
+                step === 1 && (<><input type="radio" /></>)
+            }
+
+            {
+                step === 2 && (<><input type="radio" /></>)
+            }
+
+            {
+                step === 3 && (<><input type="radio" /></>)
+            }
+
+            
         </form>
     )
 }
