fc56c1f0228d4d1de3c7f3fea0bc03ad14268c8f
Nildem Buse Ertürk <nildembuse@gmail.com>
2024-01-05 13:47:15 +0300

    duzenlemeler yapiliyor

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -6,7 +6,7 @@ export default  function Content() {
         <div className="values">
                <div className="values_text">
                     <h3>Kaliteli Hizmet Al</h3>
-                    <p>Teklif aldığın hizmet verenlerin kalitesini daha önceki işlerinde aldıkları <br /> gerçek müşteri yorumları sayesinde görür, güvenle kararını verirsin.</p>
+                    <p>Teklif aldığın hizmet verenlerin kalitesini <br /> daha önceki işlerinde aldıkları gerçek müşteri <br /> yorumları sayesinde görür, güvenle kararını verirsin.</p>
                </div>
                <div className="values_image">
                     <img src="/src/img/qualty.jpg" alt="" />
@@ -14,7 +14,7 @@ export default  function Content() {
           
                <div className="values_text">
                     <h3>Zaman Kazan</h3>
-                    <p>Aradığın hizmet için dükkan dükkan dolaşmak ya da tanıdıklarından referans aramakla uğraşma. Teklifler cebine gelsin, zamanın sevdiklerinle sana kalsın.</p>
+                    <p>Aradığın hizmet için dükkan dükkan dolaşmak ya da <br /> tanıdıklarından referans aramakla uğraşma. <br /> Teklifler cebine gelsin, zamanın sevdiklerinle sana kalsın.</p>
                </div>
                <div className="values_image">
                     <img src="/src/img/anlaşma.jpg" alt="" />
@@ -22,7 +22,7 @@ export default  function Content() {
            
                <div className="values_text">
                     <h3>Garantide Ol</h3>
-                    <p>Gönül rahatlığıyla hizmet alman için, Armut'tan teklif seçimi yaptığın işler garantimiz altında</p>
+                    <p>Gönül rahatlığıyla hizmet alman için, <br /> Armut'tan teklif seçimi yaptığın işler garantimiz altında</p>
                </div>
                <div className="values_image">
                     <img src="/src/img/choose-best-time.jpg" alt="" />
@@ -30,7 +30,7 @@ export default  function Content() {
          
                <div className="values_text">
                     <h3>Kolayca Kullan</h3>
-                    <p>Hizmet alacağın servis için özel hazırlanmış sorularına bir dakika içinde cevap ver, bekleyen işlerini kolayca hallet.</p>
+                    <p>Hizmet alacağın servis için özel hazırlanmış <br /> sorularına bir dakika içinde cevap ver, bekleyen işlerini kolayca hallet.</p>
                 </div>
                 <div className="values_image">
                     <img src="/src/img/save-time.jpg" alt="" />
